ad96cb4a2823d22c3f64a4020ad1ebe2f8545989
Ryan Block <rblock@gmail.com>
2020-05-06 19:55:57 -0700

    Use body parser

--- src/events/my-event/index.js
+++ src/events/my-event/index.js
@@ -1,18 +1,14 @@
-const queryString = require('querystring')
 const arc = require('@architect/functions')
 const data = require('@begin/data')
 const table = 'interactions'
 const key = 'clicks'
 
 async function myEvent(event) {
-  let raw = queryString.parse(
-    new Buffer.from(event.body, 'base64').toString()
-  )
-  let prop = raw.name
-  let count = await data.incr({
+  let { name } = event
+  await data.incr({
     table,
     key,
-    prop
+    prop: name
   })
 
   return
--- src/http/post-my_event/index.js
+++ src/http/post-my_event/index.js
@@ -2,7 +2,7 @@ const arc = require('@architect/functions')
 
 exports.handler = async function http (req) {
   const name = 'my-event'
-  const payload = { body: req.body }
+  const payload = arc.http.helpers.bodyParser(req)
   await arc.events.publish({ name, payload })
   return {
     statusCode: 302,
